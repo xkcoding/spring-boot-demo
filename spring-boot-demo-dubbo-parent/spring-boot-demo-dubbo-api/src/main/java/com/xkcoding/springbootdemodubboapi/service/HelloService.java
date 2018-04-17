1d38dd7363f43210f974b4b2fb0ea882cb548579
Yangkai.Shen <237497819@qq.com>
2018-04-17 18:55:13 +0800

    Spring Boot 与 Dubbo 整合抽取的服务接口

---
+++ spring-boot-demo-dubbo-parent/spring-boot-demo-dubbo-api/src/main/java/com/xkcoding/springbootdemodubboapi/service/HelloService.java
@@ -0,0 +1,18 @@
+package com.xkcoding.springbootdemodubboapi.service;
+
+/**
+ * <p>
+ * HelloService
+ * </p>
+ *
+ * @package: com.xkcoding.springbootdemodubboapi.service
+ * @description： HelloService
+ * @author: yangkai.shen
+ * @date: Created in 2018/4/17 下午5:13
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+public interface HelloService {
+	String hello(String name);
+}
