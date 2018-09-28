3162ac39f075e1b5902ad67c2598af0cc759af33
Yangkai.Shen <237497819@qq.com>
2018-09-28 15:01:00 +0800

    :sparkles: spring-boot-demo-helloworld 完成

---
+++ spring-boot-demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplication.java
@@ -0,0 +1,44 @@
+package com.xkcoding.helloworld;
+
+import cn.hutool.core.util.StrUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * SpringBoot启动类
+ * </p>
+ *
+ * @package: com.xkcoding.helloworld
+ * @description: SpringBoot启动类
+ * @author: yangkai.shen
+ * @date: Created in 2018/9/28 2:49 PM
+ * @copyright: Copyright (c)
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@SpringBootApplication
+@RestController
+public class SpringBootDemoHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoHelloworldApplication.class, args);
+	}
+
+	/**
+	 * Hello，World
+	 *
+	 * @param who 参数，非必须
+	 * @return Hello, ${who}
+	 */
+	@GetMapping("/hello")
+	public String sayHello(@RequestParam(required = false, name = "who") String who) {
+		if (StrUtil.isBlank(who)) {
+			who = "World";
+		}
+		return StrUtil.format("Hello, {}!", who);
+	}
+}
