efe45cbfd5dca8f35f0e6b1f9355129f243a05cc
Yangkai.Shen <237497819@qq.com>
2018-10-10 13:17:26 +0800

    :sparkles: spring-boot-demo-template-beetl 完成

---
+++ spring-boot-demo-template-beetl/src/test/java/com/xkcoding/template/beetl/SpringBootDemoTemplateBeetlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xkcoding.template.beetl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoTemplateBeetlApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
