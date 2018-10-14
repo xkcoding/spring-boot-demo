d7e3777af9d4a8c42186dfabfbe571502b632906
Yangkai.Shen <237497819@qq.com>
2018-10-14 23:05:44 +0800

    :tada: 初始化 spring-boot-demo-orm-jdbctemplate

---
+++ spring-boot-demo-orm-jdbctemplate/src/main/java/com/xkcoding/orm/jdbctemplate/SpringBootDemoOrmJdbctemplateApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.orm.jdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoOrmJdbctemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoOrmJdbctemplateApplication.class, args);
+    }
+}
