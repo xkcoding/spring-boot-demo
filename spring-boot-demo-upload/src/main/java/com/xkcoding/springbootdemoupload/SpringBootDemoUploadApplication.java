54b753fa89b1fcf2b988ba50cbf05e052a6b84d5
Yangkai.Shen <237497819@qq.com>
2018-10-18 19:12:00 +0800

    :sparkles: spring-boot-demo-upload 初始化

---
+++ spring-boot-demo-upload/src/main/java/com/xkcoding/springbootdemoupload/SpringBootDemoUploadApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.springbootdemoupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoUploadApplication.class, args);
+	}
+}
