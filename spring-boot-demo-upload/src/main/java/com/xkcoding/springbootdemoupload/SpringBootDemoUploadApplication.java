8b2101426686f2089617d86bbbbc851b3251fad7
Yangkai.Shen <237497819@qq.com>
2018-10-20 21:47:12 +0800

    :bento: Adding upload assets.

--- spring-boot-demo-upload/src/main/java/com/xkcoding/springbootdemoupload/SpringBootDemoUploadApplication.java
+++ spring-boot-demo-upload/src/main/java/com/xkcoding/springbootdemoupload/SpringBootDemoUploadApplication.java
@@ -3,10 +3,23 @@ package com.xkcoding.springbootdemoupload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动类
+ * </p>
+ *
+ * @package: com.xkcoding.springbootdemoupload
+ * @description: 启动类
+ * @author: shenyangkai
+ * @date: Created in 2018/10/20 21:23
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: shenyangkai
+ */
 @SpringBootApplication
 public class SpringBootDemoUploadApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoUploadApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoUploadApplication.class, args);
+    }
 }
---
+++ spring-boot-demo-upload/src/main/java/com/xkcoding/springbootdemoupload/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.xkcoding.springbootdemoupload.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * <p>
+ * 首页Controller
+ * </p>
+ *
+ * @package: com.xkcoding.springbootdemoupload.controller
+ * @description: 首页Controller
+ * @author: shenyangkai
+ * @date: Created in 2018/10/20 21:22
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: shenyangkai
+ */
+@Controller
+public class IndexController {
+    @GetMapping("")
+    public String index() {
+        return "index";
+    }
+}
