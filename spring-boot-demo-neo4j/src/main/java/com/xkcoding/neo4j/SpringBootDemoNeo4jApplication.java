a757fcfe3d76b77c1720b7050cf27eb3a166c78e
Yangkai.Shen <237497819@qq.com>
2018-10-24 21:59:00 +0800

    :tada: 初始化 spring-boot-demo-neo4j

---
+++ spring-boot-demo-neo4j/src/main/java/com/xkcoding/neo4j/SpringBootDemoNeo4jApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoNeo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoNeo4jApplication.class, args);
+    }
+}
---
+++ spring-boot-demo-neo4j/src/test/java/com/xkcoding/neo4j/SpringBootDemoNeo4jApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xkcoding.neo4j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoNeo4jApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
