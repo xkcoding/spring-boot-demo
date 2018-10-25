e895a1ba9693f359d310938badcc5d44aab50140
Yangkai.Shen <237497819@qq.com>
2018-10-25 22:23:20 +0800

    :sparkles: spring-boot-demo-elasticsearch 初始化

---
+++ spring-boot-demo-elasticsearch/src/main/java/com/xkcoding/elasticsearch/SpringBootDemoElasticsearchApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoElasticsearchApplication.class, args);
+	}
+}
