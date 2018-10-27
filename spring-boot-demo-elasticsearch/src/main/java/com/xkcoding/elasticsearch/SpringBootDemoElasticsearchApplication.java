efe5d616de301b86d1d250692dbb9055774bf777
Yangkai.Shen <237497819@qq.com>
2018-10-27 22:52:50 +0800

    :bulb: Documenting source code.

--- spring-boot-demo-elasticsearch/src/main/java/com/xkcoding/elasticsearch/SpringBootDemoElasticsearchApplication.java
+++ spring-boot-demo-elasticsearch/src/main/java/com/xkcoding/elasticsearch/SpringBootDemoElasticsearchApplication.java
@@ -3,6 +3,19 @@ package com.xkcoding.elasticsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动类
+ * </p>
+ *
+ * @package: com.xkcoding.elasticsearch
+ * @description: 启动类
+ * @author: yangkai.shen
+ * @date: Created in 2018/10/27 22:52
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoElasticsearchApplication {
 
