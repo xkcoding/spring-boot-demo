2894c0bb921a50ffe135c9922e7220e3e45c6f01
Yangkai.Shen <237497819@qq.com>
2018-10-28 22:58:38 +0800

    :bulb: Documenting source code.

--- spring-boot-demo-orm-jpa/src/main/java/com/xkcoding/orm/jpa/SpringBootDemoOrmJpaApplication.java
+++ spring-boot-demo-orm-jpa/src/main/java/com/xkcoding/orm/jpa/SpringBootDemoOrmJpaApplication.java
@@ -3,10 +3,23 @@ package com.xkcoding.orm.jpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动类
+ * </p>
+ *
+ * @package: com.xkcoding.orm.jpa
+ * @description: 启动类
+ * @author: yangkai.shen
+ * @date: Created in 2018/10/28 22:58
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoOrmJpaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoOrmJpaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoOrmJpaApplication.class, args);
+	}
 }
