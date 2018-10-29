47063675b7ca7147ae7688f54b85a749204b1c6d
Yangkai.Shen <237497819@qq.com>
2018-10-29 18:39:00 +0800

    :sparkles: spring-boot-demo-war 初始化

---
+++ spring-boot-demo-war/src/main/java/com/xkcoding/war/SpringBootDemoWarApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoWarApplication.class, args);
+	}
+}
