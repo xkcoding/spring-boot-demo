f5f7acc40997577e3b7f4fc4c453304300c55676
Yangkai.Shen <237497819@qq.com>
2018-10-30 19:38:53 +0800

    :bulb: Documenting source code.

--- spring-boot-demo-war/src/main/java/com/xkcoding/war/SpringBootDemoWarApplication.java
+++ spring-boot-demo-war/src/main/java/com/xkcoding/war/SpringBootDemoWarApplication.java
@@ -3,10 +3,23 @@ package com.xkcoding.war;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.war
+ * @description: 启动器
+ * @author: shenyangkai
+ * @date: Created in 2018/10/30 19:37
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: shenyangkai
+ */
 @SpringBootApplication
 public class SpringBootDemoWarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoWarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoWarApplication.class, args);
+    }
 }
