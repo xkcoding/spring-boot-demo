785b04c15668fa49843382ecd0980ae48e43cb9c
Yangkai.Shen <237497819@qq.com>
2018-10-31 22:18:07 +0800

    :sparkles: spring-boot-demo-war 完成

--- spring-boot-demo-war/src/main/java/com/xkcoding/war/SpringBootDemoWarApplication.java
+++ spring-boot-demo-war/src/main/java/com/xkcoding/war/SpringBootDemoWarApplication.java
@@ -2,6 +2,8 @@ package com.xkcoding.war;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * <p>
@@ -17,9 +19,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @modified: shenyangkai
  */
 @SpringBootApplication
-public class SpringBootDemoWarApplication {
+public class SpringBootDemoWarApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoWarApplication.class, args);
+    }
+
+    /**
+     * 若需要打成 war 包，则需要写一个类继承 {@link SpringBootServletInitializer} 并重写 {@link SpringBootServletInitializer#configure(SpringApplicationBuilder)}
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootDemoWarApplication.class);
     }
 }
