efc65d224451fa815135e4d94c44411695e282c8
Yangkai.Shen <237497819@qq.com>
2018-11-09 18:55:40 +0800

    :sparkles: spring-boot-demo-orm-beetlsql 初始化

---
+++ spring-boot-demo-orm-beetlsql/src/main/java/com/xkcoding/orm/beetlsql/SpringBootDemoOrmBeetlsqlApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.orm.beetlsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoOrmBeetlsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoOrmBeetlsqlApplication.class, args);
+    }
+}
