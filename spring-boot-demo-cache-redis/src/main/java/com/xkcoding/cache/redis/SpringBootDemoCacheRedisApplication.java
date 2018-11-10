1c578654cbcab9207b4e650177cc9e6b6597f88f
Yangkai.Shen <237497819@qq.com>
2018-11-10 23:24:10 +0800

    :sparkles: spring-boot-demo-cache-redis 初始化

---
+++ spring-boot-demo-cache-redis/src/main/java/com/xkcoding/cache/redis/SpringBootDemoCacheRedisApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.cache.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoCacheRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoCacheRedisApplication.class, args);
+    }
+}
