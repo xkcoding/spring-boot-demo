e80fe16ae2a724205cb507c29e7fc0f218c5eb26
Yangkai.Shen <237497819@qq.com>
2018-11-19 19:48:07 +0800

    :tada: spring-boot-demo-task-quartz 初始化.

---
+++ spring-boot-demo-task-quartz/src/main/java/com/xkcoding/task/quartz/SpringBootDemoTaskQuartzApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.task.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoTaskQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoTaskQuartzApplication.class, args);
+    }
+}
