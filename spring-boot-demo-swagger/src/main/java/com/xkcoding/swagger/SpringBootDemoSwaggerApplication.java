589991d9277a7a549a3378cde46cdb5cfbcfe985
Yangkai.Shen <237497819@qq.com>
2018-11-20 23:21:12 +0800

    :tada: spring-boot-demo-swagger 初始化.

---
+++ spring-boot-demo-swagger/src/main/java/com/xkcoding/swagger/SpringBootDemoSwaggerApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoSwaggerApplication.class, args);
+    }
+}
