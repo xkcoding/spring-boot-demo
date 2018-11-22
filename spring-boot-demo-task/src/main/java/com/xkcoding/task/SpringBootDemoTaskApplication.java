1dcf51350e3af0d5e97fb506933909986d57f321
Yangkai.Shen <237497819@qq.com>
2018-11-22 19:33:16 +0800

    :sparkles: spring-boot-demo-task 完成

--- spring-boot-demo-task/src/main/java/com/xkcoding/task/SpringBootDemoTaskApplication.java
+++ spring-boot-demo-task/src/main/java/com/xkcoding/task/SpringBootDemoTaskApplication.java
@@ -3,6 +3,19 @@ package com.xkcoding.task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.task
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2018/11/22 19:00
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoTaskApplication {
 
---
+++ spring-boot-demo-task/src/main/java/com/xkcoding/task/job/TaskJob.java
@@ -0,0 +1,52 @@
+package com.xkcoding.task.job;
+
+import cn.hutool.core.date.DateUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * <p>
+ * 定时任务
+ * </p>
+ *
+ * @package: com.xkcoding.task.job
+ * @description: 定时任务
+ * @author: yangkai.shen
+ * @date: Created in 2018/11/22 19:09
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@Component
+@Slf4j
+public class TaskJob {
+
+    /**
+     * 按照标准时间来算，每隔 10s 执行一次
+     */
+    @Scheduled(cron = "0/10 * * * * ?")
+    public void job1() {
+        log.info("【job1】开始执行：{}", DateUtil.formatDateTime(new Date()));
+    }
+
+    /**
+     * 从启动时间开始，间隔 2s 执行
+     * 固定间隔时间
+     */
+    @Scheduled(fixedRate = 2000)
+    public void job2() {
+        log.info("【job2】开始执行：{}", DateUtil.formatDateTime(new Date()));
+    }
+
+    /**
+     * 从启动时间开始，延迟 5s 后间隔 4s 执行
+     * 固定等待时间
+     */
+    @Scheduled(fixedDelay = 4000, initialDelay = 5000)
+    public void job3() {
+        log.info("【job3】开始执行：{}", DateUtil.formatDateTime(new Date()));
+    }
+}
