570157f33816a76209613848641bf3fecdb1be77
Yangkai.Shen <237497819@qq.com>
2018-11-28 17:44:23 +0800

    :sparkles: spring-boot-demo-swagger-beauty 完成

---
+++ spring-boot-demo-swagger-beauty/src/main/java/com/xkcoding/swagger/beauty/controller/UserController.java
@@ -0,0 +1,94 @@
+package com.xkcoding.swagger.beauty.controller;
+
+import com.battcn.boot.swagger.model.DataType;
+import com.battcn.boot.swagger.model.ParamType;
+import com.xkcoding.swagger.beauty.common.ApiResponse;
+import com.xkcoding.swagger.beauty.entity.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+/**
+ * <p>
+ * User Controller
+ * </p>
+ *
+ * @package: com.xkcoding.swagger.beauty.controller
+ * @description: User Controller
+ * @author: yangkai.shen
+ * @date: Created in 2018-11-28 14:25
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@RestController
+@RequestMapping("/user")
+@Api(tags = "1.0.0-SNAPSHOT", description = "用户管理", value = "用户管理")
+@Slf4j
+public class UserController {
+    @GetMapping
+    @ApiOperation(value = "条件查询（DONE）", notes = "备注")
+    @ApiImplicitParams({@ApiImplicitParam(name = "username", value = "用户名", dataType = DataType.STRING, paramType = ParamType.QUERY, defaultValue = "xxx")})
+    public ApiResponse<User> getByUserName(String username) {
+        log.info("多个参数用  @ApiImplicitParams");
+        return ApiResponse.<User>builder().code(200).message("操作成功").data(new User(1, username, "JAVA")).build();
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "主键查询（DONE）", notes = "备注")
+    @ApiImplicitParams({@ApiImplicitParam(name = "id", value = "用户编号", dataType = DataType.INT, paramType = ParamType.PATH)})
+    public ApiResponse<User> get(@PathVariable Integer id) {
+        log.info("单个参数用  @ApiImplicitParam");
+        return ApiResponse.<User>builder().code(200).message("操作成功").data(new User(id, "u1", "p1")).build();
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "删除用户（DONE）", notes = "备注")
+    @ApiImplicitParam(name = "id", value = "用户编号", dataType = DataType.INT, paramType = ParamType.PATH)
+    public void delete(@PathVariable Integer id) {
+        log.info("单个参数用 ApiImplicitParam");
+    }
+
+    @PostMapping
+    @ApiOperation(value = "添加用户（DONE）")
+    public User post(@RequestBody User user) {
+        log.info("如果是 POST PUT 这种带 @RequestBody 的可以不用写 @ApiImplicitParam");
+        return user;
+    }
+
+    @PostMapping("/multipar")
+    @ApiOperation(value = "添加用户（DONE）")
+    public List<User> multipar(@RequestBody List<User> user) {
+        log.info("如果是 POST PUT 这种带 @RequestBody 的可以不用写 @ApiImplicitParam");
+
+        return user;
+    }
+
+    @PostMapping("/array")
+    @ApiOperation(value = "添加用户（DONE）")
+    public User[] array(@RequestBody User[] user) {
+        log.info("如果是 POST PUT 这种带 @RequestBody 的可以不用写 @ApiImplicitParam");
+        return user;
+    }
+
+    @PutMapping("/{id}")
+    @ApiOperation(value = "修改用户（DONE）")
+    public void put(@PathVariable Long id, @RequestBody User user) {
+        log.info("如果你不想写 @ApiImplicitParam 那么 swagger 也会使用默认的参数名作为描述信息 ");
+    }
+
+    @PostMapping("/{id}/file")
+    @ApiOperation(value = "文件上传（DONE）")
+    public String file(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+        log.info(file.getContentType());
+        log.info(file.getName());
+        log.info(file.getOriginalFilename());
+        return file.getOriginalFilename();
+    }
+}
