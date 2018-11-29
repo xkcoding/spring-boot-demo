e663789af18dd5e1638d52d59c5922ffb467a8ff
Yangkai.Shen <237497819@qq.com>
2018-11-29 17:12:59 +0800

    :sparkles: spring-boot-demo-docker 完成

---
+++ spring-boot-demo-docker/src/main/java/com/xkcoding/docker/SpringBootDemoDockerApplication.java
@@ -0,0 +1,25 @@
+package com.xkcoding.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.docker
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2018-11-29 14:59
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@SpringBootApplication
+public class SpringBootDemoDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoDockerApplication.class, args);
+    }
+}
