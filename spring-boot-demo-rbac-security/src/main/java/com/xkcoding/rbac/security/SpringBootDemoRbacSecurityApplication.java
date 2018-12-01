2c2448ba5c671cf3635a033f0703be91045d4f5a
Yangkai.Shen <237497819@qq.com>
2018-12-01 22:17:46 +0800

    :tada: spring-boot-demo-rbac-security 初始化

---
+++ spring-boot-demo-rbac-security/src/main/java/com/xkcoding/rbac/security/SpringBootDemoRbacSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.rbac.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoRbacSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoRbacSecurityApplication.class, args);
+    }
+}
