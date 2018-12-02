352b344f729a72c5395f9a9466ebb1c277ef8a71
Yangkai.Shen <237497819@qq.com>
2018-12-02 22:55:18 +0800

    :tada: spring-boot-demo-rbac-shiro 初始化

---
+++ spring-boot-demo-rbac-shiro/src/main/java/com/xkcoding/rbac/shiro/SpringBootDemoRbacShiroApplication.java
@@ -0,0 +1,12 @@
+package com.xkcoding.rbac.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoRbacShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoRbacShiroApplication.class, args);
+    }
+}
---
+++ spring-boot-demo-rbac-shiro/src/test/java/com/xkcoding/rbac/shiro/SpringBootDemoRbacShiroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xkcoding.rbac.shiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoRbacShiroApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
