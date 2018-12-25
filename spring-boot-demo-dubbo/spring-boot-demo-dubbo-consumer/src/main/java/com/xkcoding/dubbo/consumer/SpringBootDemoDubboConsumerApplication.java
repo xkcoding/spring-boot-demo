b74893650e685f59eff5461081196e7ecb2e496e
Yangkai.Shen <237497819@qq.com>
2018-12-25 16:55:04 +0800

    :tada: spring-boot-demo-dubbo 初始化

---
+++ spring-boot-demo-dubbo/spring-boot-demo-dubbo-consumer/src/main/java/com/xkcoding/dubbo/consumer/SpringBootDemoDubboConsumerApplication.java
@@ -0,0 +1,25 @@
+package com.xkcoding.dubbo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.dubbo.consumer
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2018-12-25 16:49
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@SpringBootApplication
+public class SpringBootDemoDubboConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoDubboConsumerApplication.class, args);
+    }
+}
