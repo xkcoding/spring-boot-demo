6c204b7f5610a2664682570221a2784848a99570
Yangkai.Shen <237497819@qq.com>
2018-12-26 15:32:53 +0800

    :tada: spring-boot-demo-mongodb 初始化

---
+++ spring-boot-demo-mongodb/src/main/java/com/xkcoding/mongodb/SpringBootDemoMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoMongodbApplication.class, args);
+    }
+
+}
+
