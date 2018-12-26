f2e0d5b4f4ffcb5aa50522ea55d629317665251a
Yangkai.Shen <237497819@qq.com>
2018-12-26 15:15:33 +0800

    :tada: spring-boot-demo-zookeeper 初始化

---
+++ spring-boot-demo-zookeeper/src/main/java/com/xkcoding/zookeeper/SpringBootDemoZookeeperApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoZookeeperApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-demo-zookeeper/src/test/java/com/xkcoding/zookeeper/SpringBootDemoZookeeperApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xkcoding.zookeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoZookeeperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
