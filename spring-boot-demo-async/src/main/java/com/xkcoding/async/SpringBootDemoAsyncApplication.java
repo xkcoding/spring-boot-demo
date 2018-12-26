aa6397740de6ee50cb4d76b750a006030f13f905
Yangkai.Shen <237497819@qq.com>
2018-12-26 15:32:05 +0800

    :tada: spring-boot-demo-async 初始化

---
+++ spring-boot-demo-async/src/main/java/com/xkcoding/async/SpringBootDemoAsyncApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoAsyncApplication.class, args);
+    }
+
+}
+
