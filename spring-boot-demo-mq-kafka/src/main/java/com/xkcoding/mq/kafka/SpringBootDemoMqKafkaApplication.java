4639d8544b604af39be134dd9cd11d32a2defcbb
Yangkai.Shen <237497819@qq.com>
2018-12-26 15:42:04 +0800

    :tada: spring-boot-demo-mq-kafka 初始化

---
+++ spring-boot-demo-mq-kafka/src/main/java/com/xkcoding/mq/kafka/SpringBootDemoMqKafkaApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.mq.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoMqKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoMqKafkaApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-demo-mq-kafka/src/test/java/com/xkcoding/mq/kafka/SpringBootDemoMqKafkaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xkcoding.mq.kafka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoMqKafkaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
