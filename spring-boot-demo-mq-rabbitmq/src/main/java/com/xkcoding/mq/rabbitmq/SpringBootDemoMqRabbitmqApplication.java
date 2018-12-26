4a6af3b055e58c55653e65a286a94d1512aa69e7
Yangkai.Shen <237497819@qq.com>
2018-12-26 15:34:39 +0800

    :tada: spring-boot-demo-mq-rabbitmq 初始化

---
+++ spring-boot-demo-mq-rabbitmq/src/main/java/com/xkcoding/mq/rabbitmq/SpringBootDemoMqRabbitmqApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.mq.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoMqRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoMqRabbitmqApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-demo-mq-rabbitmq/src/test/java/com/xkcoding/mq/rabbitmq/SpringBootDemoMqRabbitmqApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xkcoding.mq.rabbitmq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoMqRabbitmqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
