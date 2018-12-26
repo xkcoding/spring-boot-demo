6e0e3302bb64aa0af8f0f68635df0b55fc45a50b
Yangkai.Shen <237497819@qq.com>
2018-12-26 15:45:28 +0800

    :tada: spring-boot-demo-urule 初始化

---
+++ spring-boot-demo-urule/src/main/java/com/xkcoding/urule/SpringBootDemoUruleApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.urule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoUruleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoUruleApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-demo-urule/src/test/java/com/xkcoding/urule/SpringBootDemoUruleApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xkcoding.urule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoUruleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
