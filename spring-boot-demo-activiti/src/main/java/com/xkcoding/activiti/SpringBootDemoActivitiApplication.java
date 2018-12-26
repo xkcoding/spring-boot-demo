11ebcc59f54468c6f8e0dc623b32c45e60a0e614
Yangkai.Shen <237497819@qq.com>
2018-12-26 15:31:08 +0800

    :tada: spring-boot-demo-activiti 初始化

---
+++ spring-boot-demo-activiti/src/main/java/com/xkcoding/activiti/SpringBootDemoActivitiApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoActivitiApplication.class, args);
+    }
+
+}
+
