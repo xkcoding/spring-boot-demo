3b8a864299b328fee4a296c2af3a357ab5ac573d
Yangkai.Shen <237497819@qq.com>
2018-12-27 18:10:33 +0800

    :sparkles: spring-boot-demo-zookeeper 分布式锁 基本完成

--- spring-boot-demo-zookeeper/src/main/java/com/xkcoding/zookeeper/SpringBootDemoZookeeperApplication.java
+++ spring-boot-demo-zookeeper/src/main/java/com/xkcoding/zookeeper/SpringBootDemoZookeeperApplication.java
@@ -3,6 +3,19 @@ package com.xkcoding.zookeeper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.zookeeper
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2018-12-27 14:51
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoZookeeperApplication {
 
