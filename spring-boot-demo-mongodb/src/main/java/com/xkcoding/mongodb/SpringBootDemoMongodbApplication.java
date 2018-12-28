b7c981911d18c0333f8dcb6502a8fd3ca5715e84
Yangkai.Shen <237497819@qq.com>
2018-12-28 17:43:58 +0800

    :sparkles: spring-boot-demo-mongodb 完成

--- spring-boot-demo-mongodb/src/main/java/com/xkcoding/mongodb/SpringBootDemoMongodbApplication.java
+++ spring-boot-demo-mongodb/src/main/java/com/xkcoding/mongodb/SpringBootDemoMongodbApplication.java
@@ -1,13 +1,34 @@
 package com.xkcoding.mongodb;
 
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.mongodb
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2018-12-28 16:14
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoMongodbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoMongodbApplication.class, args);
+    }
+
+    @Bean
+    public Snowflake snowflake() {
+        return IdUtil.createSnowflake(1, 1);
     }
 
 }
