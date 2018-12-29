0e84a22a06ab260a9e1b2539fa43e4c78b3ea861
Yangkai.Shen <237497819@qq.com>
2018-12-29 11:07:01 +0800

    :sparkles: spring-boot-demo-async 完成

--- spring-boot-demo-async/src/main/java/com/xkcoding/async/SpringBootDemoAsyncApplication.java
+++ spring-boot-demo-async/src/main/java/com/xkcoding/async/SpringBootDemoAsyncApplication.java
@@ -2,7 +2,22 @@ package com.xkcoding.async;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.async
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2018-12-29 10:28
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@EnableAsync
 @SpringBootApplication
 public class SpringBootDemoAsyncApplication {
 
