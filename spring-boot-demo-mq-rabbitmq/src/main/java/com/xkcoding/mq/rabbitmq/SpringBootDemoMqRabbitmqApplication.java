b8a4bc487a2872bdfdcbb724f01b62897be267cf
Yangkai.Shen <237497819@qq.com>
2018-12-30 22:13:15 +0800

    :construction: spring-boot-demo-mq-rabbitmq 努力ing

--- spring-boot-demo-mq-rabbitmq/src/main/java/com/xkcoding/mq/rabbitmq/SpringBootDemoMqRabbitmqApplication.java
+++ spring-boot-demo-mq-rabbitmq/src/main/java/com/xkcoding/mq/rabbitmq/SpringBootDemoMqRabbitmqApplication.java
@@ -3,6 +3,19 @@ package com.xkcoding.mq.rabbitmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.mq.rabbitmq
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2018-12-29 13:58
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoMqRabbitmqApplication {
 
