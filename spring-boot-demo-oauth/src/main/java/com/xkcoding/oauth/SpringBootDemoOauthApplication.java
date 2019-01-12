4ce81afe5eb79ff8ecf539772e18c423bd8a221e
Yangkai.Shen <237497819@qq.com>
2019-01-12 20:02:20 +0800

    :tada: spring-boot-demo-oauth 初始化

---
+++ spring-boot-demo-oauth/src/main/java/com/xkcoding/oauth/SpringBootDemoOauthApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoOauthApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-demo-oauth/src/test/java/com/xkcoding/oauth/SpringBootDemoOauthApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xkcoding.oauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoOauthApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
