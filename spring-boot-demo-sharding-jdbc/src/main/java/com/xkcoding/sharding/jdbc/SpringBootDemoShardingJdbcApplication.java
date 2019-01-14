be87264a4c731fc37c6c1d7dffd7c983400b171c
Yangkai.Shen <237497819@qq.com>
2019-01-14 09:54:16 +0800

    :tada: spring-boot-demo-sharding-jdbc 初始化

---
+++ spring-boot-demo-sharding-jdbc/src/main/java/com/xkcoding/sharding/jdbc/SpringBootDemoShardingJdbcApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.sharding.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoShardingJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoShardingJdbcApplication.class, args);
+    }
+
+}
+
---
+++ spring-boot-demo-sharding-jdbc/src/test/java/com/xkcoding/sharding/jdbc/SpringBootDemoShardingJdbcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xkcoding.sharding.jdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoShardingJdbcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
