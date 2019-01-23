1013970a539790a9ba69e7b860aa4eeec53fe794
Yangkai.Shen <237497819@qq.com>
2019-01-23 22:06:13 +0800

    :tada: spring-boot-demo-sharding-jdbc 初始化

--- spring-boot-demo-sharding-jdbc/src/main/java/com/xkcoding/sharding/jdbc/SpringBootDemoShardingJdbcApplication.java
+++ spring-boot-demo-sharding-jdbc/src/main/java/com/xkcoding/sharding/jdbc/SpringBootDemoShardingJdbcApplication.java
@@ -3,6 +3,19 @@ package com.xkcoding.sharding.jdbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.sharding.jdbc
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2019-01-23 22:05
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoShardingJdbcApplication {
 
