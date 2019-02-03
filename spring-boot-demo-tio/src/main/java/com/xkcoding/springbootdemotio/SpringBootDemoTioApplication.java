79ddca41661cc1fc382f14e07341d5ff2979856a
Yangkai.Shen <237497819@qq.com>
2019-02-03 23:23:37 +0800

    :tada: spring-boot-demo-tio 初始化

---
+++ spring-boot-demo-tio/src/main/java/com/xkcoding/springbootdemotio/SpringBootDemoTioApplication.java
@@ -0,0 +1,14 @@
+package com.xkcoding.springbootdemotio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoTioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoTioApplication.class, args);
+    }
+
+}
+
