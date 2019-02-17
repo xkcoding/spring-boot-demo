b91b0b6c6753073354b206ce1d8e15a08d374b92
Yangkai.Shen <237497819@qq.com>
2019-02-17 23:53:27 +0800

    :tada: spring-boot-demo-oauth 初始化

--- spring-boot-demo-oauth/src/main/java/com/xkcoding/oauth/SpringBootDemoOauthApplication.java
+++ spring-boot-demo-oauth/src/main/java/com/xkcoding/oauth/SpringBootDemoOauthApplication.java
@@ -3,6 +3,19 @@ package com.xkcoding.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.oauth
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2019-02-17 23:52
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoOauthApplication {
 
