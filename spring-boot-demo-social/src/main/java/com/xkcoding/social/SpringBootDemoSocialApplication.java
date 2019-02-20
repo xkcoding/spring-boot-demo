164d795b7430670519dfcbdc88ef9e11a9697057
Yangkai.Shen <237497819@qq.com>
2019-02-20 17:12:55 +0800

    :tada: spring-boot-demo-social 初始化

--- spring-boot-demo-social/src/main/java/com/xkcoding/social/SpringBootDemoSocialApplication.java
+++ spring-boot-demo-social/src/main/java/com/xkcoding/social/SpringBootDemoSocialApplication.java
@@ -3,6 +3,19 @@ package com.xkcoding.social;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @package: com.xkcoding.social
+ * @description: 启动器
+ * @author: yangkai.shen
+ * @date: Created in 2019-02-19 16:04
+ * @copyright: Copyright (c) 2019
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
 @SpringBootApplication
 public class SpringBootDemoSocialApplication {
 
