18ef7daa7e4fe1c2115669a61116567e768de0de
Yangkai.Shen <237497819@qq.com>
2019-04-22 23:03:15 +0800

    :tada: spring-boot-demo-graylog 初始化完成

---
+++ spring-boot-demo-graylog/src/main/java/com/xkcoding/graylog/SpringBootDemoGraylogApplication.java
@@ -0,0 +1,13 @@
+package com.xkcoding.graylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoGraylogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoGraylogApplication.class, args);
+    }
+
+}
