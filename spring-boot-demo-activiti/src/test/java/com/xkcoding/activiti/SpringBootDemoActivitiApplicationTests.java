f7ab2121bc14c6184e4d90cc0f145fcd9f66df72
Yangkai.Shen <237497819@qq.com>
2019-07-01 18:59:26 +0800

    :sparkles: activiti 测试

--- spring-boot-demo-activiti/src/test/java/com/xkcoding/activiti/SpringBootDemoActivitiApplicationTests.java
+++ spring-boot-demo-activiti/src/test/java/com/xkcoding/activiti/SpringBootDemoActivitiApplicationTests.java
@@ -1,7 +1,13 @@
 package com.xkcoding.activiti;
 
+import com.xkcoding.activiti.util.SecurityUtil;
+import org.activiti.api.process.model.ProcessDefinition;
+import org.activiti.api.process.runtime.ProcessRuntime;
+import org.activiti.api.runtime.shared.query.Page;
+import org.activiti.api.runtime.shared.query.Pageable;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +15,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBootDemoActivitiApplicationTests {
 
+    @Autowired
+    private ProcessRuntime processRuntime;
+    @Autowired
+    private SecurityUtil securityUtil;
+
     @Test
     public void contextLoads() {
+        securityUtil.logInAs("salaboy");
+        Page<ProcessDefinition> processDefinitionPage = processRuntime.processDefinitions(Pageable.of(0, 10));
+        processDefinitionPage.getContent().forEach(System.out::println);
     }
 
 }
