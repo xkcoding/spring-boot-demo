2644612ef3e3371c554ae1ee2339b9992583f25d
Yangkai.Shen <237497819@qq.com>
2019-09-11 20:20:01 +0800

    :tada: 使用 Guava 限流 demo 初始化

---
+++ spring-boot-demo-ratelimit-guava/README.md
@@ -0,0 +1,2 @@
+# spring-boot-demo-ratelimit-guava
+
--- spring-boot-demo-ratelimit-guava/src/main/java/com/xkcoding/ratelimit/guava/SpringBootDemoRatelimitGuavaApplication.java
+++ spring-boot-demo-ratelimit-guava/src/main/java/com/xkcoding/ratelimit/guava/SpringBootDemoRatelimitGuavaApplication.java
@@ -3,6 +3,14 @@ package com.xkcoding.ratelimit.guava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019/9/12 14:06
+ */
 @SpringBootApplication
 public class SpringBootDemoRatelimitGuavaApplication {
 
