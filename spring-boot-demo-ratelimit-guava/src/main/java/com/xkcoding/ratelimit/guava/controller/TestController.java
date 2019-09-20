f10c69f19ff6a05095c32103777c65a4c2872bdd
Yangkai.Shen <237497819@qq.com>
2019-09-20 09:04:16 +0800

    :sparkles: spring-boot-demo-ratelimit-guava 完成，测试不同方法限流

--- spring-boot-demo-ratelimit-guava/src/main/java/com/xkcoding/ratelimit/guava/controller/TestController.java
+++ spring-boot-demo-ratelimit-guava/src/main/java/com/xkcoding/ratelimit/guava/controller/TestController.java
@@ -30,4 +30,11 @@ public class TestController {
         log.info("【test2】被执行了。。。。。");
         return Dict.create().set("msg", "hello,world!").set("description", "我一直都在，卟离卟弃");
     }
+
+    @RateLimiter(value = 2.0, timeout = 300)
+    @GetMapping("/test3")
+    public Dict test3() {
+        log.info("【test3】被执行了。。。。。");
+        return Dict.create().set("msg", "hello,world!").set("description", "别想一直看到我，不信你快速刷新看看~");
+    }
 }
