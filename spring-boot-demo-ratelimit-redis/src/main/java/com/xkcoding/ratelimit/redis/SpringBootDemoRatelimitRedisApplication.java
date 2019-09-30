575fa4338c32fdd3c5c1452a139b6604e5dcdddf
Yangkai.Shen <237497819@qq.com>
2019-09-30 15:20:52 +0800

    :tada: spring-boot-demo-ratelimit-redis 初始化

---
+++ spring-boot-demo-ratelimit-redis/src/main/java/com/xkcoding/ratelimit/redis/SpringBootDemoRatelimitRedisApplication.java
@@ -0,0 +1,21 @@
+package com.xkcoding.ratelimit.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date Created in 2019/9/30 09:32
+ */
+@SpringBootApplication
+public class SpringBootDemoRatelimitRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoRatelimitRedisApplication.class, args);
+    }
+
+}
