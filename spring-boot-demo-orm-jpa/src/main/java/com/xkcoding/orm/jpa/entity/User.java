796f7b9e23b2c213b485f405dba6d40d74b8c3bf
76peter <412044923@qq.com>
2019-10-03 13:38:36 +0800

    修改注释描述

--- spring-boot-demo-orm-jpa/src/main/java/com/xkcoding/orm/jpa/entity/User.java
+++ spring-boot-demo-orm-jpa/src/main/java/com/xkcoding/orm/jpa/entity/User.java
@@ -79,7 +79,7 @@ public class User extends AbstractAuditModel {
     //3、inverseJoinColumns指定外键的名字，要关联的关系被维护端(Department)
     //4、其实可以不使用@JoinTable注解，默认生成的关联表名称为主表表名+下划线+从表表名，
     //即表名为user_department
-    //关联到主表的外键名：主表名+下划线+主表中的主键列名,即user_id
+    //关联到主表的外键名：主表名+下划线+主表中的主键列名,即user_id,这里使用referencedColumnName指定
     //关联到从表的外键名：主表中用于关联的属性名+下划线+从表的主键列名,department_id
     //主表就是关系维护端对应的表，从表就是关系被维护端对应的表
     private Collection<Department> departmentList;
