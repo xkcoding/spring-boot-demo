fe29d66a30c89a359c03ee6505ebe6fe1fff3090
sunyuhu0301 <1351089670@qq.com>
2020-12-30 17:19:13 +0800

    增加knife4j-swagger 聚合项目

---
+++ spring-boot-demo-sharding-jdbc/src/main/java/com/xkcoding/sharding/jdbc/service/OrderService.java
@@ -0,0 +1,20 @@
+package com.xkcoding.sharding.jdbc.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xkcoding.sharding.jdbc.model.Order;
+
+/**
+ * <p>
+ * User Service
+ * </p>
+ *
+ * @package: com.xkcoding.orm.mybatis.plus.service
+ * @description: User Service
+ * @author: yangkai.shen
+ * @date: Created in 2018/11/8 18:10
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+public interface OrderService extends IService<Order> {
+}
