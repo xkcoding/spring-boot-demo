e6e11bebdbe06f3e5f756502f7c448747aef7939
sunyuhu0301 <1351089670@qq.com>
2020-12-30 17:17:40 +0800

    增加knife4j-swagger 聚合项目

--- spring-boot-demo-swagger-knife4j/src/main/java/com/xkcoding/nife4j/config/Knife4jConfiguration.java
+++ spring-boot-demo-swagger-knife4j/src/main/java/com/xkcoding/nife4j/config/Knife4jConfiguration.java
@@ -24,7 +24,7 @@ public class Knife4jConfiguration {
                 .version("1.0")
                 .build())
             //分组名称
-            .groupName("2.X版本")
+            .groupName("2.test")
             .select()
             //这里指定Controller扫描包路径
             .apis(RequestHandlerSelectors.basePackage("com.xkcoding.nife4j.controller"))
