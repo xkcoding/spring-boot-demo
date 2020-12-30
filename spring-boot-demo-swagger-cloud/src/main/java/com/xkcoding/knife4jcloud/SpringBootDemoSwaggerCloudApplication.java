c61f732fc881f466ed20dae198b6c2c9923fb926
sunyuhu0301 <1351089670@qq.com>
2020-12-30 17:17:40 +0800

    增加knife4j-swagger 聚合项目

---
+++ spring-boot-demo-swagger-cloud/src/main/java/com/xkcoding/knife4jcloud/SpringBootDemoSwaggerCloudApplication.java
@@ -0,0 +1,13 @@
+package com.xkcoding.knife4jcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoSwaggerCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoSwaggerCloudApplication.class, args);
+    }
+
+}
---
+++ spring-boot-demo-swagger-cloud/src/test/java/com/xkcoding/knife4jcloud/SpringBootDemoSwaggerCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xkcoding.knife4jcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoSwaggerCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
