eaea6126d4f0f703f7d3d0c6239431b0faaff657
Yangkai.Shen <237497819@qq.com>
2022-10-11 10:41:19 +0800

    :tada: init multilevel cache.

---
+++ demo-cache/demo-cache-multilevel/src/main/java/com/xkcoding/cache/multilevel/MultilevelCacheApplication.java
@@ -0,0 +1,19 @@
+package com.xkcoding.cache.multilevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 启动器
+ * </p>
+ *
+ * @author yangkai.shen
+ * @date 2022-10-11 10:40
+ */
+@SpringBootApplication
+public class MultilevelCacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultilevelCacheApplication.class, args);
+    }
+}
